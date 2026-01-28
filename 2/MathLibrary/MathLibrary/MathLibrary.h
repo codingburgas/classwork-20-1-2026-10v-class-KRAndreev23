ec8d1fe1bae32a1d7de8c8f9425ada0cedf5d30a
KRAndreev23 <KRAndreev23@codingburgas.bg>
2026-01-28 23:20:22 +0200

    Add 2nd task

---
+++ 2/MathLibrary/MathLibrary/MathLibrary.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void TrianglePerimeter(int a, int b, int c);
+
+void TrianglePlot(int a, int b);
+
---
+++ 2/MathLibrary/StudentWork/StudentWork.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "..\MathLibrary\MathLibrary.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
