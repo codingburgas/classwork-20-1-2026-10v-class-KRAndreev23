f26b12454597660498d4a8c09860c50a2e810734
KRAndreev23 <KRAndreev23@codingburgas.bg>
2026-01-28 23:10:54 +0200

    Add 1st task

---
+++ 1/MathLibrary/MathLibrary/MathLibrary.cpp
@@ -0,0 +1,23 @@
+
+
+#include "pch.h"
+#include "framework.h"
+#include "MathLibrary.h"
+#include <iostream>
+
+using namespace std;
+
+
+void TrianglePerimeter(int a, int b, int c)
+{
+
+	cout << a + b + c;
+
+}
+
+void TrianglePlot(int a, int b)
+{
+
+	cout << (a + b) / 2;
+
+}
